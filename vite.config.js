5390ea483c9c35830992087c6098e448fd054602
Monalisha Sahu <mona.2005.lisha@gmail.com>
2026-01-16 10:25:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Expense-tracker/',   // ⭐ THIS IS THE KEY
   plugins: [
     react(),
     tailwindcss(),
